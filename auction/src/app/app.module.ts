f81d3d9ef294cdcde9c657cab0ea1c79685019d0
Gasonzhong <gasonzhong@163.com>
2017-08-29 20:14:27 +0800

    angular2股票网站学习
    组件完成

---
+++ auction/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
+import { StartsComponent } from './starts/starts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    FooterComponent,
+    StartsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
